480e26e8a8c08895a55228d158df80d3979ece1d
William Ashton <William@AshtonFam.org>
2015-06-15 01:27:39 -0400

    Store a price with each clothing type

--- lib/clothes.rb
+++ lib/clothes.rb
@@ -1,13 +1,18 @@
 class Article
-  attr_reader :type, :fabric_type, :color
+  attr_reader :type, :fabric_type, :color, :price
   
   def initialize(new_type, new_fabric_type, new_color)
     @type = new_type
     @fabric_type = new_fabric_type
     @color = new_color
+    @base_price = 0
+  end
+  
+  def price
+    "$#{@base_price}"
   end
   
   def to_s
-    "#{@color} #{@type}"
+    "#{@color} #{@type} for #{price}"
   end
 end
