dc58bac9a2025e50c866e3bec2583f07ac42f415
William Ashton <William@AshtonFam.org>
2015-07-03 00:52:35 -0400

    Add Style Number

--- lib/styles.rb
+++ lib/styles.rb
@@ -1,5 +1,5 @@
 class Style
-  attr_reader :type, :fabric_type, :color
+  attr_reader :type, :fabric_type, :color, :style_number
 
   @@types = [:shirt, :blouse, :skirt, :shorts, :pants, :dress, :jumpsuit, :sweater]
   @@fabric_types = [:cotton, :silk, :spandex, :rayon, :polyester, :velvet]
@@ -7,9 +7,14 @@ class Style
   @@prices = [1, 2, 3, 4, 15]
 
   def initialize()
-    @type = @@types.sample
-    @fabric_type = @@fabric_types.sample
-    @color = @@colors.sample
+    g = Random.new
+    
+    @type = g.rand @@types.length
+    @fabric_type = g.rand @@fabric_types.length
+    @color = g.rand @@colors.length
+    
+    @style_number = "#{@type}#{@fabric_type}#{@color}"
+    
     @base_price = @@prices.sample
   end
 
@@ -22,10 +27,10 @@ class Style
   end
 
   def to_s
-    "#{@color} #{@type}"
+    "#{@@colors[@color]} #{@@types[@type]}"
   end
 
   def sales_tag
-    "#{@color} #{@type} for #{price}"
+    "#{style_number}: #{@@colors[@color]} #{@@types[@type]} for #{price}"
   end
 end
