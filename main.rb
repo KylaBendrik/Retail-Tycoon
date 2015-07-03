2545af7335df44c98553cdc4a7b5deba765f7f5a
Beth Ashton <Beth@AshtonFam.org>
2015-07-02 22:51:34 -0400

    I made a loooooop to ask for commands

--- main.rb
+++ main.rb
@@ -3,16 +3,18 @@ require_relative 'lib/inventory'
 prices = $inventory.map(&:price)
 if prices.include? "$0"
   puts "You have unpriced items!"
-  
+
   $inventory.select { |style| style.price == "$0" }.each do |style|
     puts "What price do you want for #{style}"
     style.price = gets.chomp
   end
 end
 
-puts "Display inventory? (Yn)"
-command = gets
+loop do
+  puts "What do you want to do? (For list of commands, type 'h')"
+  command = gets.chomp
 
-if command.chomp == 'Y' or command == "\n"
-  puts $inventory.map(&:sales_tag)
+  if command == 'h'
+    puts "no commands available (except, of course, 'h')"
+  end
 end
